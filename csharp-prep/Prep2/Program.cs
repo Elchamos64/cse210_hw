f58a8aca94f16d4783720ef760dc69ffb7482d98
Elchamos64 <ram22018@byui.edu>
2023-09-18 11:15:45 -0600

    1st commit

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        string letterGrade = "";
+
+         if (percentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
     }
 }
