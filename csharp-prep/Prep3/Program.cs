29732df5d08b77a59882ebeb191325b5199fb130
Elchamos64 <ram22018@byui.edu>
2023-09-25 10:56:39 -0600

    guess the number commit

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Console.WriteLine("Welcome to the magic number game! Guess a number between 1 and 100! ");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        int guess = 1;
+
+        while ( guess != magicNumber)
+        {
+            Console.Write("What is your guess ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Too high!");
+            }
+
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else
+            {
+                Console.WriteLine($"You got it right! The answer was " +magicNumber );
+            }
+        }
     }
 }
